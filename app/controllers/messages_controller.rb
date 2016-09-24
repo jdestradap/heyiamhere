50b857cdde2e7bdbeb661dc138c6bdf47335b4df
jdestradap <jdestradap@gmail.com>
2016-09-24 18:32:41 -0500

    get all messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,11 @@
 class MessagesController < ApplicationController
   respond_to :json
 
+  def index
+    @messages = Message.all
+    respond_with @messages, status: 200    
+  end
+
   def create
     @message = Message.new(message_params)    
     if @message.save
