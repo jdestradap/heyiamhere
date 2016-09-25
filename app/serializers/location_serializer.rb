598888b69215a3b314e03938e2d0d3d5f8b0c7c6
jdestradap <jdestradap@gmail.com>
2016-09-25 11:56:28 -0500

    add fields to location serializer

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,5 +1,4 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :tracking, :start_date, :end_date
+  attributes :id, :tracking, :start_date, :end_date, :latitud, :longitud, :max_radius_alert
   belongs_to :article
-
 end
