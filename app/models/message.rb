9ba3509706ecb4d51df4cc023669083ea2327489
jdestradap <jdestradap@gmail.com>
2016-09-24 22:01:11 -0500

    insert list of messages.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ApplicationRecord
   belongs_to :article
 
+  def self.bulk_messages(messages)
+    print "hi"
+    puts  messages.inspect
+    print "hi2"    
+    messages.each do |message|
+#     self.create(article_id: message.article_id , motion: message.motion, latitud: message.latitud, longitud: message.longitud, radius: message.radius, message_time: message.message_time)
+    end
+  end
 end
