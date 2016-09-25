b179d523ec4b3446c2605322ba79643d4e32a453
pablopaho <papatino@sura.com.co>
2016-09-25 11:55:52 -0500

    new post articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,11 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def create
+    @article = Article.new(params[:id])
+    @article.save    
+  end
+  
   def show
     @article = Article.find(params[:id])
     respond_with @article, status: 200
