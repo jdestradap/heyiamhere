7ed287b7af946dd0332c2ba904620aec97c4a640
jdestradap <jdestradap@gmail.com>
2016-09-25 14:48:11 -0500

    service to get messages given article id.

--- app/controllers/articles/messages_controller.rb
+++ app/controllers/articles/messages_controller.rb
@@ -2,11 +2,7 @@ class Articles::MessagesController < ApplicationController
   respond_to :json
 
   def index
-    print "hellooooo"
-    @message = Article.find(params[:article_id]).messages.last
-    print "hi"
-    puts @message.inspect
-    print "hi 2"
-    respond_with @message, status: 200
+    @messages = Article.find(params[:article_id]).messages
+    respond_with @messages, status: 200
   end
 end
