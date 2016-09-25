90b2f7e1af4a61dbccc12edc39f8debb64b556ba
jdestradap <jdestradap@gmail.com>
2016-09-25 10:44:18 -0500

    migration and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_bike = User.create(name: "Jeronimo", cellphone: "+573128485660", email:"jzc723@gmail.com", created_at: Time.now, updated_at: Time.now)
+user_son = User.create(name: "Jubel", cellphone: "+573006205791", email:"jubel@gmail.com", created_at: Time.now, updated_at: Time.now)
+
+bike = Article.create(name: "Trek", description: "La Zanahoria", created_at: Time.now, updated_at: Time.now)
+
+jubelcito = Article.create(name: "Hijo", description: "Hijo", created_at: Time.now, updated_at: Time.now)
+
+user_bike.articles << bike
+user_son.articles << jubelcito
